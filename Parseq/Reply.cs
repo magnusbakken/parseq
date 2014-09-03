9ccc8f326888962dbe99a6833167cfdc34273934
linerlock <linerlock@outlook.com>
2014-09-03 20:56:04 +0900

    Add Reply, Reply.Spec
    Add Reply, Reply.Spec
    Add ReplyExtensions (NotImplemented)
    Add ReplyExtensions.Spec (Not Implemented)

---
+++ Parseq/Reply.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * Copyright (C) 2012 - 2014 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+using System;
+
+namespace Parseq
+{
+    public interface IReply<TToken, TResult>
+        : IEither<String, TResult>
+    {
+        IStream<TToken> RestStream { get; }
+
+        new T Case<T>(Func<String, T> failure, Func<TResult, T> success);
+    }
+
+    public static partial class Reply
+    {
+        public static IReply<TToken, T> Success<TToken, T>(IStream<TToken> restStream, T value)
+        {
+            return new Reply.SuccessImpl<TToken, T>(restStream, value);
+        }
+
+        public static IReply<TToken, T> Failure<TToken, T>(IStream<TToken> restStream, String errorMessage)
+        {
+            return new Reply.FailureImpl<TToken, T>(restStream, errorMessage);
+        }
+    }
+
+    public static partial class Reply
+    {
+        class SuccessImpl<TToken, TResult>
+            : IReply<TToken, TResult>
+        {
+            public SuccessImpl(IStream<TToken> restStream, TResult value)
+            {
+                if (restStream == null)
+                    throw new ArgumentNullException("restStream");
+
+                this.RestStream = restStream;
+                this.Value = value;
+            }
+
+            public IStream<TToken> RestStream
+            {
+                get;
+                private set;
+            }
+
+            public TResult Value
+            {
+                get;
+                private set;
+            }
+
+            T IReply<TToken, TResult>.Case<T>(Func<string, T> failure, Func<TResult, T> success)
+            {
+                if (failure == null)
+                    throw new ArgumentNullException("failure");
+                if (success == null)
+                    throw new ArgumentNullException("success");
+
+                return success(this.Value);
+            }
+
+            T IEither<string, TResult>.Case<T>(Func<string, T> left, Func<TResult, T> right)
+            {
+                if (left == null)
+                    throw new ArgumentNullException("left");
+                if (right == null)
+                    throw new ArgumentNullException("right");
+
+                return right(this.Value);
+            }
+        }
+
+        class FailureImpl<TToken, TResult>
+            : IReply<TToken, TResult>
+        {
+            public FailureImpl(IStream<TToken> restStream, String errorMessage)
+            {
+                if (restStream == null)
+                    throw new ArgumentNullException("restStream");
+                if (errorMessage == null)
+                    throw new ArgumentNullException("errorMessage");
+
+                this.RestStream = restStream;
+                this.ErrorMessage = errorMessage;
+            }
+
+            public IStream<TToken> RestStream
+            {
+                get;
+                private set;
+            }
+
+            public String ErrorMessage
+            {
+                get;
+                private set;
+            }
+
+            T IReply<TToken, TResult>.Case<T>(Func<string, T> failure, Func<TResult, T> success)
+            {
+                if (failure == null)
+                    throw new ArgumentNullException("failure");
+                if (success == null)
+                    throw new ArgumentNullException("success");
+
+                return failure(this.ErrorMessage);
+            }
+
+            T IEither<string, TResult>.Case<T>(Func<string, T> left, Func<TResult, T> right)
+            {
+                if (left == null)
+                    throw new ArgumentNullException("left");
+                if (right == null)
+                    throw new ArgumentNullException("right");
+
+                return left(this.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ Test/Reply.Spec.fs
@@ -0,0 +1,49 @@
+﻿(*
+ * Copyright (C) 2012 - 2014 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ *)
+module Test.Reply
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open System
+open System.Linq
+open Parseq
+
+open FsUnit.MsTest
+
+[<TestClass>]
+type Spec () =
+    [<TestMethod>] member test.
+     ``Parseq.Reply.Failure(_, errorMessage).Case(failure, success) retuns failure(errorMessage)`` () =
+        let ``don't care`` = new Parseq.CharStream(new System.IO.StringReader("don't care")) in
+        let errorMessage = "foobar" in
+        let failure = Func<string, string>(fun x -> x) in
+        let success = Func<string, string>(fun _ -> "don't care") in
+        Parseq.Reply.Failure(``don't care``, errorMessage).Case(failure, success)
+            |> should equal (failure.Invoke(errorMessage))
+
+    [<TestMethod>] member test.
+     ``Parseq.Reply.Success(_, value).Case(failure, success) retuns success(value)`` () =
+        let ``don't care`` = new Parseq.CharStream(new System.IO.StringReader("don't care")) in
+        let value = 42 in
+        let failure = Func<string, int>(fun _ -> -1) in
+        let success = Func<int, int>(fun x -> x) in
+        Parseq.Reply.Success(``don't care``, value).Case(failure, success)
+            |> should equal (success.Invoke(value))
